72d291038a8500e2d4090e4d3e8ecea3ba0b7e82
Yair_Blad <yair.dev.py@gmail.com>
2025-03-30 21:55:54 -0600

    obteniendo los 10 empleados mas productivos

--- rentabilidad.py
+++ rentabilidad.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 conn = sqlite3.connect("Nortwind.db")
 
 #Obteniendo los 10 productos mas rentables y graficandolos
-query = '''
+query_prod = '''
     SELECT ProductName, SUM(Price * Quantity) as Revenue
     FROM OrderDetails od
     JOIN Products p ON p.ProductID = od.ProductID
@@ -14,12 +14,34 @@ query = '''
     LIMIT 10
 '''
 
-top_products = pd.read_sql_query(query, conn)
+top_products = pd.read_sql_query(query_prod, conn)
 
-top_products.plot(x="ProductName", y="Revenue", kind="bar", figsize=(10, 5), legend=False)
+top_products.plot(x="ProductName", y="Revenue", kind="bar", figsize=(10, 6), legend=False)
 
 plt.title("10 Productos mas rentables")
 plt.xlabel("Productos")
 plt.ylabel("Revenue")
 plt.xticks(rotation=80)
+plt.show()
+
+
+#Obteniendo los 10 empleados mas productivos
+
+query_employee = '''
+    SELECT FirstName || " " || LastName as Employee, COUNT(*) as Total
+    FROM Orders o
+    JOIN Employees e
+    ON e.EmployeeID = o.EmployeeID
+    GROUP BY o.EmployeeID
+    ORDER BY Total DESC
+    LIMIT 10
+'''
+
+top_employees = pd.read_sql_query(query_employee, conn)
+top_employees.plot(x="Employee", y="Total", kind="bar", figsize=(10, 6), legend=False)
+
+plt.title("10 Empleados mas efectivos")
+plt.xlabel("Empleados")
+plt.ylabel("Total vendido")
+plt.xticks(rotation = 80)
 plt.show()
